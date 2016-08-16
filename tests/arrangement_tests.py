12278d839f2e7dc82a93fc30b1242967a8904501
Eric Ihli <ericihli@gmail.com>
2016-08-16 00:15:01 -0700

    Change 2 spaces to 4

--- tests/arrangement_tests.py
+++ tests/arrangement_tests.py
@@ -5,8 +5,8 @@ from ..participant import Participant
 import os
 
 class arrangementTestCase(unittest.TestCase):
-  def setUp(self):
-    self.arrangement = Arrangement(filename = os.path.abspath(os.path.join('grouper/sample_data/affinities.json')))
+    def setUp(self):
+        self.arrangement = Arrangement(filename = os.path.abspath(os.path.join('grouper/sample_data/affinities.json')))
     self.arrangement2 = Arrangement()
     self.p1 = Participant("Eric")
     self.p2 = Participant("Sam")
@@ -24,40 +24,40 @@ class arrangementTestCase(unittest.TestCase):
 
 
   def test_arrangement_exists(self):
-    self.assertIsInstance(self.arrangement, Arrangement)
+      self.assertIsInstance(self.arrangement, Arrangement)
 
   def test_arrangement_has_groups(self):
-    self.assertIsInstance(self.arrangement.groups, list)
+      self.assertIsInstance(self.arrangement.groups, list)
 
   def test_arrangement_has_participants(self):
-    self.assertIsInstance(self.arrangement.participants, list)
+      self.assertIsInstance(self.arrangement.participants, list)
 
   def test_arrangement_add_group(self):
-    self.arrangement.addGroup()
+      self.arrangement.addGroup()
     self.assertIsInstance(self.arrangement.groups[0], Group)
 
   def test_arrangement_get_participant(self):
-    self.arrangement.addParticipant(self.p1)
+      self.arrangement.addParticipant(self.p1)
     participant = self.arrangement.getParticipant('Eric')
     self.assertIsInstance(self.arrangement.getParticipant('Eric'), Participant)
 
   def test_arrangement_add_participant(self):
-    self.arrangement.addParticipant(self.p1)
+      self.arrangement.addParticipant(self.p1)
     self.assertIsInstance(self.arrangement.participants[0], Participant)
 
   def test_arrangement_can_add_participant_to_group(self):
-    self.arrangement.groups[0] = self.g1
+      self.arrangement.groups[0] = self.g1
     self.arrangement.addParticipantToGroup(self.p1, self.g1)
     self.assertIs(self.arrangement.groups[0].participants[0], self.p1)
     self.assertIs(self.p1.group, self.g1)
 
   def test_arrangement_can_read_from_file(self):
-    self.assertEqual(len(self.arrangement.participants), 20)
+      self.assertEqual(len(self.arrangement.participants), 20)
     self.assertIsInstance(self.arrangement.participants[1], Participant)
     self.assertEqual(self.arrangement.participants[0].name, 'Mary Polster')
 
   def test_arrangement_can_assign_participants_to_group(self):
-    a = Arrangement()
+      a = Arrangement()
     a.addParticipant(self.p1)
     a.addParticipant(self.p2)
     a.addParticipant(self.p3)
@@ -67,12 +67,12 @@ class arrangementTestCase(unittest.TestCase):
     self.assertEqual(a.groups[0].participants[0].name, 'Eric')
 
   def test_arrangement_scoring_function(self):
-    self.arrangement.groups = []
+      self.arrangement.groups = []
     group = Group()
     # TODO: Finish arrangement scoring function/tests
 
   def test_remove_participant_from_group(self):
-    a = Arrangement()
+      a = Arrangement()
     g = Group()
     a.addGroup(g)
     a.addParticipantToGroup(self.p1, g)
@@ -84,13 +84,13 @@ class arrangementTestCase(unittest.TestCase):
         a.groups[0].participants[0]
     # Yeah... len() might work too, and might be more understandable
     # without comments. But this is cool, and these are comments.
-    
+
   # These next tests are something that should be abstracted out into a strategy
   # We want to be able to swap the each person from the unhappiest group
   # into each other group until and keep the "best" arrangement from those
   # swaps.
   def test_get_unhappiest_group(self):
-    a = Arrangement()
+      a = Arrangement()
     a.addGroup()
     a.addGroup()
     p1 = Participant('eric')
@@ -113,7 +113,7 @@ class arrangementTestCase(unittest.TestCase):
   # We don't want the arrangement score checking code inside this function.
   # https://www.jeffknupp.com/blog/2013/04/07/improve-your-python-yield-and-generators-explained/
   def test_makes_best_swap_from_unhappiest_group(self):
-    self.p1.addAffinity(self.p2)
+      self.p1.addAffinity(self.p2)
     self.p1.addAffinity(self.p4)
     self.p1.addTechnicalRefusal(self.p3)
     self.p2.addAffinity(self.p3)
