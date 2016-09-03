4f2e9a63220226a73d1f530ae8f0aa2914130b40
Eric Ihli <ericihli@gmail.com>
2016-09-03 11:02:52 -0700

    Remove comments and unneded test

--- tests/arrangement_test.py
+++ tests/arrangement_test.py
@@ -3,7 +3,6 @@ from grouping_algo.arrangement import Arrangement
 from ..group import Group
 from ..participant import Participant
 import os
-import pdb
 import json
 
 class arrangementTestCase(unittest.TestCase):
@@ -33,21 +32,11 @@ class arrangementTestCase(unittest.TestCase):
         self.assertIs(self.arrangement.groups[0].participants[0], self.p1)
         self.assertIs(self.p1.group, self.g1)
 
-    def test_arrangement_can_read_from_file(self):
-        self.assertEqual(len(self.arrangement.participants), 20)
-        self.assertIsInstance(self.arrangement.participants[1], Participant)
-        names = map(lambda p: p.name, self.arrangement.participants)
-        self.assertIn('Mary Polster', names)
-
     def test_arrangement_scoring_function(self):
         self.arrangement.groups = []
         group = Group()
         # TODO: Finish arrangement scoring function/tests
 
-    # These next tests are something that should be abstracted out into a strategy
-    # We want to be able to swap the each person from the unhappiest group
-    # into each other group until and keep the "best" arrangement from those
-    # swaps.
     def test_get_unhappiest_group(self):
         json_arrangement = {
             "technical_refusals": {
@@ -72,12 +61,6 @@ class arrangementTestCase(unittest.TestCase):
         a = Arrangement(json_arrangement, 2)
         self.assertEqual(a.getUnhappiestGroup(), a.groups[1])
 
-    # I think I just found a perfect use case for generators and yield
-    # I want to loop over each person in a group, swap that person with each
-    # person in each other group, and after each swap, check to see
-    # the score of the arrangement.
-    # We don't want the arrangement score checking code inside this function.
-    # https://www.jeffknupp.com/blog/2013/04/07/improve-your-python-yield-and-generators-explained/
     def test_makes_best_swap_from_unhappiest_group(self):
         json_arrangement = {
             "technical_refusals": {
