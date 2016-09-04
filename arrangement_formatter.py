89acb52f87daf2f500e273e02f38c0f5f54713a1
Eric Ihli <ericihli@gmail.com>
2016-09-03 21:16:18 -0700

    Update formatting of csv exporter

--- arrangement_formatter.py
+++ arrangement_formatter.py
@@ -3,13 +3,29 @@ from functools import reduce
 
 class ArrangementFormatter:
     @staticmethod
-    def create_csv_from_groups(groups, output_file):
+    def create_csv_from_groups(arrangement, groups, output_file):
         csv_writer = csv.writer(output_file)
         for i, group in enumerate(groups):
             csv_writer.writerow(['Group ' + str(i) + ':', ''] + _get_group_header(group))
+            for participant in group:
+                csv_writer.writerow(_get_participant_row(participant, group, arrangement))
         return output_file 
 
 def _get_group_header(group):
     return list(map(lambda p: str(p['id']) + ': ' + p['name'], group))
 
-
+def _get_participant_row(participant, group, arrangement):
+    header_columns = _get_group_header(group)
+    row = []
+    for column in header_columns:
+        value = ''
+        target_id = int(column.split(':')[0])
+        if target_id in participant['technical_refusals']:
+            value = '-1'
+        if target_id in participant['interpersonal_refusals']:
+            value = '-1'
+        if target_id in participant['affinities']:
+            value = '1'
+        row.append(value)
+    row = ['', str(participant['id']) + ': ' + participant['name']] + row
+    return row
--- tests/arrangement_formatter_test.py
+++ tests/arrangement_formatter_test.py
@@ -11,6 +11,51 @@ class ArrangementFormatterTest(unittest.TestCase):
         if not os.path.exists(self.tmp_dir):
             os.makedirs(self.tmp_dir)
 
+        self.arrangement = [
+            {
+                'id': 0,
+                'name': 'a',
+                'technical_refusals': [3],
+                'interpersonal_refusals': [],
+                'affinities': [5]
+            },
+            {
+                'id': 1,
+                'name': 'b',
+                'technical_refusals': [],
+                'interpersonal_refusals': [],
+                'affinities': []
+            },
+            {
+                'id': 2,
+                'name': 'c',
+                'technical_refusals': [],
+                'interpersonal_refusals': [5],
+                'affinities': []
+            },
+            {
+                'id': 3,
+                'name': 'd',
+                'technical_refusals': [0],
+                'interpersonal_refusals': [],
+                'affinities': [2]
+            },
+            {
+                'id': 4,
+                'name': 'e',
+                'technical_refusals': [],
+                'interpersonal_refusals': [],
+                'affinities': []
+            },
+            {
+                'id': 5,
+                'name': 'f',
+                'technical_refusals': [2],
+                'interpersonal_refusals': [],
+                'affinities': []
+            }
+        ]
+
         self.groups = [
             [
                 {'affinities': [5], 'interpersonal_refusals': [], 'id': 0, 'technical_refusals': [3], 'name': 'a'},
@@ -29,7 +74,7 @@ class ArrangementFormatterTest(unittest.TestCase):
 
     def test_export_groups_to_csv(self):
         with open(self.tmp_dir + '/output.csv', 'w') as f:
-            csv_file = ArrangementFormatter.create_csv_from_groups(self.groups, f)
+            csv_file = ArrangementFormatter.create_csv_from_groups(self.arrangement, self.groups, f)
             self.assertTrue(hasattr(csv_file, 'read'), "Expected csv to be an instance of file but it doesnt' have the 'read' method.")
 
         with open(self.tmp_dir + '/output.csv', 'r') as f:
@@ -38,5 +83,14 @@ class ArrangementFormatterTest(unittest.TestCase):
             self.assertEqual(first_line, ['Group 0:', '', '0: a', '3: d'])
 
             second_line = next(csv_reader)
-            self.assertEqual(second_line, ['Group 1:', '', '1: b', '4: e'])
+            self.assertEqual(second_line, ['', '0: a', '', '-1'])
+
+            third_line = next(csv_reader)
+            self.assertEqual(third_line, ['', '3: d', '-1', ''])
+
+            fourth_line = next(csv_reader)
+            self.assertEqual(fourth_line, ['Group 1:', '', '1: b', '4: e'])
+
+            fifth_line = next(csv_reader)
+            self.assertEqual(fifth_line, ['', '1: b', '', ''])
 
