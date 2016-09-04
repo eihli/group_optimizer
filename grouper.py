ae975ee95e8918b54c41320b46e3f22ae89d05c9
Eric Ihli <ericihli@gmail.com>
2016-09-03 23:08:51 -0700

    Add main grouping class

--- grouper.py
+++ grouper.py
@@ -1,8 +1,7 @@
 import time
 import logging
-from . import csv2json
-from . import arrangement2csv
 from .arrangement import Arrangement
+from .arrangement_formatter import ArrangementFormatter
 
 NUM_ITERATIONS = 10
 TIMEOUT = 200
@@ -17,6 +16,16 @@ logger.setLevel(logging.DEBUG)
 class Grouper:
     def __init__(self):
         self.csv = None
+
+    @staticmethod
+    def group(input_filename, output_filename):
+        with open(input_filename, 'r') as survey_responses:
+            arrangement_dict = ArrangementFormatter.create_arrangement_from_csv(survey_responses)
+            arrangement = Arrangement(arrangement_dict, 2)
+            arrangement.optimize()
+
+        with open(output_filename, 'w') as csv_file:
+            ArrangementFormatter.create_csv_from_groups(arrangement.groups, csv_file)
 
 def swap_unhappiest(arrangement):
     previous_score_improvement = float('inf')
--- tests/grouper_test.py
+++ tests/grouper_test.py
@@ -1,9 +1,37 @@
 import unittest
+import time
+import os
+import csv
+import shutil
 from ..grouper import Grouper
 
-class GroupitTestCase(unittest.TestCase):
+class GrouperTestCase(unittest.TestCase):
     def setUp(self):
-        self.grouper = Grouper()
+        self.tmp_dir = 'tmp' + str(time.time())
+        if not os.path.exists(self.tmp_dir):
+            os.makedirs(self.tmp_dir)
 
-    def test_groupit_exists(self):
-        self.assertIsInstance(self.grouper, Grouper)
+        with open(self.tmp_dir + '/input.csv', 'w') as f:
+            csv_writer = csv.writer(f)
+            csv_writer.writerow(['', 'a', 'b', 'c', 'd', 'e', 'f'])
+            csv_writer.writerow(['a', '', '', '', '-1', '', '1'])
+            csv_writer.writerow(['b', '', '', '', '', '-1', ''])
+            csv_writer.writerow(['c', '', '', '', '1', '', ''])
+            csv_writer.writerow(['d', '-1', '', '', '', '', ''])
+            csv_writer.writerow(['e', '', '', '', '', '', ''])
+            csv_writer.writerow(['f', '1', '', '', '', '', ''])
+
+    def tearDown(self):
+        shutil.rmtree(self.tmp_dir)
+
+
+    def test_group(self):
+        Grouper.group(self.tmp_dir + '/input.csv', self.tmp_dir + '/output.csv')
+        self.assertTrue(os.path.exists(self.tmp_dir + '/output.csv'), "Expected 'group' to create output file but none existed")
+        with open(self.tmp_dir + '/output.csv', 'r') as result_file:
+            csv_reader = csv.reader(result_file)
+            first_row = next(csv_reader)
+            self.assertEqual(first_row, ['Group 0:', '', '0: a', '5: f'])
+
+            second_row = next(csv_reader)
+            self.assertEqual(second_row, ['', '0: a', '', '1'])
