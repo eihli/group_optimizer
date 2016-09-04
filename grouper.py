887cf457d7f6a6965e0ef245c0e85849c99eab7e
Eric Ihli <ericihli@gmail.com>
2016-09-04 09:31:08 -0700

    Remove unused grouper methods

--- grouper.py
+++ grouper.py
@@ -32,28 +32,3 @@ class Grouper:
         with open(output_filename, 'w') as csv_file:
             ArrangementFormatter.create_csv_from_groups(arrangement.groups, csv_file)
 
-def swap_unhappiest(arrangement):
-    previous_score_improvement = float('inf')
-    arrangement.randomizeGroups()
-    start_time = end_time = time.time()
-    while (end_time - start_time < TIMEOUT and
-            previous_score_improvement != 0):
-        previous_score_improvement = arrangement.makeBestSwap()
-        logger.debug(arrangement)
-        print(previous_score_improvement)
-        arrangement.score = arrangement.calculateScore()
-    return arrangement
-
-def perform(csv_file):
-    json_arrangement = csv2json(csv_file)
-
-    arrangements = [Arrangement(jsonString = json_arrangement)
-                    for x in range(NUM_ITERATIONS)]
-
-    results = []
-    for arrangement in arrangements:
-        results.append(swap_unhappiest(arrangement))
-        results = sorted(results, key = lambda x: x.score)
-        logger.debug(results[-3:])
-
-    arrangement2csv(results[-1], csv_file + '_result.csv')
