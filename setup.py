d989bef68dc038c843ec5cdc13f3d05982a89cc0
Eric Ihli <ericihli@gmail.com>
2016-10-10 21:50:58 -0700

    Add setup.py and dist/
    Now installable with running install
    running build
    running install_egg_info
    Removing /Users/eihli/.pyenv/versions/3.5.1/lib/python3.5/site-packages/group_optimizer-0.1-py3.5.egg-info
    Writing /Users/eihli/.pyenv/versions/3.5.1/lib/python3.5/site-packages/group_optimizer-0.1-py3.5.egg-info

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='group-optimizer',
+    version='0.1',
+    description='Optimal grouping utilities and algorithms.',
+    author='Eric Ihli',
+    author_email='eihli@owoga.com',
+    url='https://github.com/eihli/group-optimizer')
