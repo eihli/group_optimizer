44ef3734b7fbea332ae90054e0b64fcfd392f3ec
Eric Ihli <ericihli@gmail.com>
2016-11-28 00:55:29 +0000

    Re-arrange tests and comment for clarity

--- tests/grouper_test.py
+++ tests/grouper_test.py
@@ -8,6 +8,36 @@ from group_optimizer.grouper import Grouper
 
 class GrouperTestCase(unittest.TestCase):
     def setUp(self):
+        self.csv_file = TemporaryFile('w+')
+        csv_writer = csv.writer(self.csv_file)
+        csv_writer.writerow(['', 'a', 'b', 'c', 'd', 'e', 'f'])
+        csv_writer.writerow(['a', '', '', '', '-1', '', '1'])
+        csv_writer.writerow(['b', '', '', '', '', '-1', ''])
+        csv_writer.writerow(['c', '', '', '', '1', '', ''])
+        csv_writer.writerow(['d', '-1', '', '', '', '', ''])
+        csv_writer.writerow(['e', '', '', '', '', '', ''])
+        csv_writer.writerow(['f', '1', '', '', '', '', ''])
+        self.csv_file.seek(0)
+
+    def tearDown(self):
+        self.csv_file.close()
+
+    def test_optimize(self):
+        grouper = Grouper(self.csv_file, 2)
+        score = grouper.optimize()
+        self.assertEqual(score, -97)
+        score = grouper.optimize()
+        self.assertEqual(score, 2)
+
+class GrouperStaticTestCase(unittest.TestCase):
+    """Tests the static #group method
+
+    This method reads, optimizes, and writes to file.
+    It is being deprecated in favor of allowing more granular control
+    over how a group is optimized.
+    """
+
+    def setUp(self):
         self.tmp_dir = 'tmp' + str(time.time())
         if not os.path.exists(self.tmp_dir):
             os.makedirs(self.tmp_dir)
@@ -37,27 +67,3 @@ class GrouperTestCase(unittest.TestCase):
             second_row = next(csv_reader)
             self.assertEqual(second_row, ['', '0: a', '', '1'])
 
-class GrouperFileTestCase(unittest.TestCase):
-    def setUp(self):
-        self.out_file = TemporaryFile('w+')
-        self.csv_file = TemporaryFile('w+')
-        csv_writer = csv.writer(self.csv_file)
-        csv_writer.writerow(['', 'a', 'b', 'c', 'd', 'e', 'f'])
-        csv_writer.writerow(['a', '', '', '', '-1', '', '1'])
-        csv_writer.writerow(['b', '', '', '', '', '-1', ''])
-        csv_writer.writerow(['c', '', '', '', '1', '', ''])
-        csv_writer.writerow(['d', '-1', '', '', '', '', ''])
-        csv_writer.writerow(['e', '', '', '', '', '', ''])
-        csv_writer.writerow(['f', '1', '', '', '', '', ''])
-        self.csv_file.seek(0)
-
-    def tearDown(self):
-        self.out_file.close()
-        self.csv_file.close()
-
-    def test_optimize(self):
-        grouper = Grouper(self.csv_file, 2)
-        score = grouper.optimize()
-        self.assertEqual(score, -97)
-        score = grouper.optimize()
-        self.assertEqual(score, 2)
