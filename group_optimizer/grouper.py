2042767162ab8a4cb7b6ca7792a575543761e8af
Eric Ihli <ericihli@gmail.com>
2016-12-01 03:49:47 +0000

    Remove unused code.
    This is a breaking change.

--- group_optimizer/grouper.py
+++ group_optimizer/grouper.py
@@ -28,8 +28,6 @@ class Grouper:
     """
 
     def __init__(self,
-            in_file,
-            out_file,
             num_participants_per_group = 4,
             arrangement = None,
             strategy = None):
@@ -38,17 +36,6 @@ class Grouper:
         self.strategy = strategy or Strategy
         self.last_score = float('-inf')
         self.current_score = self.arrangement.get_score()
-
-    # TODO: Deprecate. This calls arrangement's #optimize which will do full optimization
-    @staticmethod
-    def group(input_filename, output_filename, num_participants_per_group = 4):
-        with open(input_filename, 'r') as survey_responses:
-            arrangement_dict = ArrangementFormatter.create_arrangement_from_csv(survey_responses)
-            arrangement = Arrangement(arrangement_dict, num_participants_per_group)
-            arrangement.optimize()
-
-        with open(output_filename, 'w') as csv_file:
-            ArrangementFormatter.create_csv_from_groups(arrangement.groups, csv_file)
 
     # Perform a single optimization swap
     def optimize(self):
